83d2bdc263a96cf610397ee51d97372cd7c66a4f
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2022-08-03 05:58:29 -0300

    Usando VStack

--- example-swiftui/ContentView.swift
+++ example-swiftui/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Example SwiftUI")
+            Text("ESPECIAL")
+            Text("BRASIL")
+        }
     }
 }
 
