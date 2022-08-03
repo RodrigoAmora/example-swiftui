85c4bcbe8c0eeb4761b5db926ae8fb9bf13abe6e
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2022-08-03 04:36:42 -0300

    Estrutura do projeto

---
+++ example-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  example-swiftui
+//
+//  Created by Rodrigo Amora on 03/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ example-swiftui/example_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  example_swiftuiApp.swift
+//  example-swiftui
+//
+//  Created by Rodrigo Amora on 03/08/22.
+//
+
+import SwiftUI
+
+@main
+struct example_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
