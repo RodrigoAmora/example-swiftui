c4ba5337a92ac39ddac96a2fd571e88bdfc5ef10
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2022-08-09 02:41:18 -0300

    Aumentando a altura do heder e colocando padding no título

--- example-swiftui/ContentView.swift
+++ example-swiftui/ContentView.swift
@@ -9,32 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Rodrigo Viagens")
-                .foregroundColor(Color.white)
-                .font(.custom("Avenir Black", size: 20))
-            
-            Text("ESPECIAL")
-                .foregroundColor(Color.white)
-                .font(.custom("Avenir Book", size: 20))
-                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 30)
-            
-            Text("BRASIL")
-                .foregroundColor(Color.white)
-                .font(.custom("Avenir Black", size: 20))
-                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
-                .padding(.leading, 30)
-            
-            List {
-                Text("Rio de Janeiro")
-                Text("Ceará")
-                Text("Porto Alegre")
-                Text("Manaus")
+        GeometryReader { view in
+            VStack {
+                VStack {
+                    Text("Rodrigo Viagens")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 20))
+                        .padding(.top, 50)
+                    
+                    Text("ESPECIAL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Book", size: 20))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                    
+                    Text("BRASIL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 23))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                }
+                .frame(width: view.size.width, height: 180, alignment: .top)
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.purple/*@END_MENU_TOKEN@*/)
+                .edgesIgnoringSafeArea(.all)
+                
+                List {
+                    Text("Rio de Janeiro")
+                    Text("Ceará")
+                    Text("Porto Alegre")
+                    Text("Manaus")
+                }
             }
         }
-        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.purple/*@END_MENU_TOKEN@*/)
-        .edgesIgnoringSafeArea(.all)
     }
 }
 
