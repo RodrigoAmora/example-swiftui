cde0eca6c6404ce0890f2261e9db48e5eb1c9301
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2022-08-11 16:45:14 -0300

    Adicionando o GeometryReader na View

--- example-swiftui/Views/HeaderView.swift
+++ example-swiftui/Views/HeaderView.swift
@@ -9,12 +9,63 @@ import SwiftUI
 
 struct HeaderView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { view in
+            VStack {
+                VStack {
+                    Text("Rodrigo Viagens")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 20))
+                        .padding(.top, 50)
+                    
+                    Text("ESPECIAL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Book", size: 20))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                    
+                    Text("BRASIL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 23))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                }
+                .frame(width: view.size.width, height: 180, alignment: .top)
+                .background(Color.purple)
+                
+                HStack {
+                    Button(action: {}) {
+                        Text("Hotéis")
+                            .font(.custom("Avenir Medium", size: 17))
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                    }
+                    .frame(width: 100, height: 50)
+                    .overlay(RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.blue, lineWidth: 10))
+                    .background(.blue)
+                    .offset(x: 50)
+                    
+                    Spacer()
+                    
+                    Button(action: {}) {
+                        Text("Pacotes")
+                            .font(.custom("Avenir Medium", size: 17))
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                    }
+                    .frame(width: 100, height: 50)
+                    .overlay(RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.orange, lineWidth: 10))
+                    .background(.orange)
+                    .offset(x: -50)
+                }
+                .offset(y: -25)
+            }
+        }
     }
 }
 
 struct HeaderView_Previews: PreviewProvider {
     static var previews: some View {
         HeaderView()
+            .previewLayout(.fixed(width: 400, height: 220))
     }
 }
