9842655ec2df5a855f54cde18f9f45cae1b744bb
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2022-08-12 00:15:53 -0300

    Criando a view MapaView

---
+++ example-swiftui/Views/MapaView.swift
@@ -0,0 +1,30 @@
+//
+//  MapaView.swift
+//  example-swiftui
+//
+//  Created by Rodrigo Amora on 11/08/22.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapaView: UIViewRepresentable {
+    
+    var coordenada:CLLocationCoordinate2D
+    
+    func makeUIView(context: Context) -> MKMapView {
+        return MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let span = MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        let region = MKCoordinateRegion(center: coordenada, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+struct MapaView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapaView(coordenada: viagens[0].localizacao)
+    }
+}
